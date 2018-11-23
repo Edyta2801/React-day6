83b1c08a4b97d1cc11c05cc25eb3240156fb5957
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-23 10:11:49 +0100

    add function deleteTask

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -1,7 +1,38 @@
 import React from 'react'
 
 class ToDo extends React.Component{
-render() {
+state={
+    tasks:[],
+    filterText:'',
+    chosenFilter:'ALL',
+    newTaskText:''
+}
+
+createTask=text=>({
+    taskText:text,
+    isCompleted:false,
+    key:Date.now()
+})
+
+
+
+addTask=()=>this.setState({
+    tasks:this.state.tasks.concat(
+        this.createTask(
+            this.state.newTaskText
+        )
+    )
+})
+deleteTask=taskKey=>this.setState({
+    tasks:this.state.tasks.filter(task=>task.key!==taskKey)
+})
+
+
+
+
+
+
+    render() {
     return(
         <div>
     ToDo
