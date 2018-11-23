d4c3f0dcc0ef28c32c3243531b78e6cef861d616
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-23 10:22:47 +0100

    completeTask with mutate task object

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -1,31 +1,43 @@
 import React from 'react'
 
-class ToDo extends React.Component{
-state={
-    tasks:[],
-    filterText:'',
-    chosenFilter:'ALL',
-    newTaskText:''
-}
+class ToDo extends React.Component {
+    state = {
+        tasks: [],
+        filterText: '',
+        chosenFilter: 'ALL',
+        newTaskText: ''
+    }
 
-createTask=text=>({
-    taskText:text,
-    isCompleted:false,
-    key:Date.now()
-})
+    createTask = text => ({
+        taskText: text,
+        isCompleted: false,
+        key: Date.now()
+    })
 
 
 
-addTask=()=>this.setState({
-    tasks:this.state.tasks.concat(
-        this.createTask(
-            this.state.newTaskText
+    addTask = () => this.setState({
+        tasks: this.state.tasks.concat(
+            this.createTask(
+                this.state.newTaskText
+            )
         )
-    )
-})
-deleteTask=taskKey=>this.setState({
-    tasks:this.state.tasks.filter(task=>task.key!==taskKey)
-})
+    })
+    deleteTask = taskKey => this.setState({
+        tasks: this.state.tasks.filter(task => task.key !== taskKey)
+    })
+
+    completeTask = taskKey => this.setState({
+        tasks: this.state.tasks.map(
+            task => {
+                if (task.key !== taskKey) {
+                    task.osCompleted = true
+                }
+                return task
+            }
+        )
+
+    })
 
 
 
@@ -33,11 +45,11 @@ deleteTask=taskKey=>this.setState({
 
 
     render() {
-    return(
-        <div>
-    ToDo
+        return (
+            <div>
+                ToDo
         </div>
-    )
+        )
     }
 }
 export default ToDo
